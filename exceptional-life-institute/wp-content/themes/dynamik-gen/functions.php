9fa03946d85b97ed15c9aa39696b5232cb48f51e
Sophia Kuo <thestatuskuo@gmail.com>
2015-08-12 00:45:25 -0700

    Upgrade Genesis

--- exceptional-life-institute/wp-content/themes/dynamik-gen/functions.php
+++ exceptional-life-institute/wp-content/themes/dynamik-gen/functions.php
@@ -15,3 +15,15 @@ require_once( get_template_directory() . '/lib/init.php' );
  * Include Dynamik theme files
  */
 require_once( get_stylesheet_directory() . '/lib/init.php' );
+
+remove_filter( 'the_content', 'wpautop' );
+remove_filter( 'the_excerpt', 'wpautop' );
+//* Customize the entire footer
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+add_action( 'genesis_footer', 'sp_custom_footer' );
+function sp_custom_footer() {
+	?>
+	<p>&copy; Copyright 2015 <a href="http://exceptionallifeinstitute.com/">Exceptional Life Institute</a> &middot; All Rights Reserved</p>
+	<?php
+}
+?>
